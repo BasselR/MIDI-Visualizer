9761d618e79f1384585e3c4f1a035f3f6f0dcb58
Bassel <mail2bassel@yahoo.com>
2020-01-02 01:24:50 -0500

    Added more JSON functions to learner file

--- jsonIntro.py
+++ jsonIntro.py
@@ -1,6 +1,6 @@
 import json
 
-with open('maybeShorter.json') as json_file:
+with open('c_scale.json') as json_file:
     song = json.load(json_file)
 
 myTracks = []
@@ -8,6 +8,8 @@ myTracks = []
 for track in song["tracks"]:
     if track["notes"]:
         myTracks = track["notes"]
+
+print(len(myTracks))
 
 for i in myTracks:
     print(i)
