a71f849292cda1b88708a79ee74be2fc1f9c3a9c
Bassel <mail2bassel@yahoo.com>
2020-01-02 01:46:46 -0500

    Cleaned up code and added comments

--- jsonIntro.py
+++ jsonIntro.py
@@ -1,15 +1,19 @@
 import json
 
-with open('c_scale.json') as json_file:
+fileName = 'maybeShorter.json'
+
+#Loads entire MIDI file as one big JSON Object into a python dictionary 
+with open(fileName) as json_file:
     song = json.load(json_file)
 
-myTracks = []
+noteList = []
 
+#Locates the "track" which contains the song's notes and makes a noteList from that array of notes
 for track in song["tracks"]:
     if track["notes"]:
-        myTracks = track["notes"]
+        noteList = track["notes"]
 
-print(len(myTracks))
+print("Parsing {0} with {1} notes\n".format(fileName, len(noteList)))
 
-for i in myTracks:
+for i in noteList:
     print(i)
