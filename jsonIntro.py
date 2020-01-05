eac2c8326b86dad86827c6fb9dfe8afcfd559141
Bassel <mail2bassel@yahoo.com>
2020-01-04 22:04:31 -0500

    Configured CLAs as the new method to choose source file

--- jsonIntro.py
+++ jsonIntro.py
@@ -1,6 +1,7 @@
 import json
+import sys
 
-fileName = 'maybeShorter.json'
+fileName = sys.argv[1]
 
 #Loads entire MIDI file as one big JSON Object into a python dictionary 
 with open(fileName) as json_file:
@@ -11,7 +12,7 @@ noteList = []
 #Locates the "track" which contains the song's notes and makes a noteList from that array of notes
 for track in song["tracks"]:
     if track["notes"]:
-        noteList = track["notes"]
+        noteList = track["notes"]   #Assign the first non-empty "note" list as the noteList
 
 print("Parsing {} with {} notes\n".format(fileName, len(noteList)))
 
